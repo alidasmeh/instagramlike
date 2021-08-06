9c89bee41bfa40b6f7f296b1acd36d61ea35fe5c
Ali Dasmeh <alidasmeh@gmail.com>
2021-08-06 09:49:09 +0430

    just frontend by ejs

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
+let express = require('express');
+let router = express.Router();
+let bcrypt = require("bcrypt"); // use this package to encrypt/hash the password (https://www.npmjs.com/package/bcryptjs) (guidance let enc_pass = await bcrypt.hash('mypass', 8))
 
 router.get('/', function(req, res, next) {
-    res.redirect("/signup")
+    res.redirect("/auth/signup")
 });
 
 router.get('/signup', function(req, res, next) {
